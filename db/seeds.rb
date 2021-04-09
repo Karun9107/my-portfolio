75518d1947629daee855c74c80f841dc5173ab6f
Karun9107 <karunrajagopal9107@gmail.com>
2021-04-09 21:43:15 +0530

    Implemented show functionality for portfolio module

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,7 +29,7 @@ puts "5 skills created"
             nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
             esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt 
             in culpa qui officia deserunt mollit anim id est laborum.",
-        main_image:"https://place-hold.it/600x4000",    
+        main_image:"https://place-hold.it/600x400",    
         thumb_image:"https://place-hold.it/350x200" 
     )
 end
