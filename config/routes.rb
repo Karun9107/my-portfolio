8d0808b13179b5e05763616165051bba835ff229
Karun9107 <karunrajagopal9107@gmail.com>
2021-04-20 17:30:33 +0530

    Implemented logout functionality and dynamic view rendering for auth

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
     get 'portfolios/angular', to: 'portfolios#angular'
     get 'portfolios/ruby_on_rails', to: 'portfolios#ruby_on_rails'
+    
   resources :blogs
 
   resources :blogs do
