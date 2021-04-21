8b3c6a9efb1b2a5697844b6e17ce0e164a15bae3
Karun9107 <karunrajagopal9107@gmail.com>
2021-04-21 22:55:15 +0530

    Implemented content tag helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,19 @@
 module ApplicationHelper
 
+    #view helper method
     def login_helper
         if current_user.is_a?(User)
             link_to 'Logout', destroy_user_session_path, method: :delete
         else
             (link_to 'Register', new_user_registration_path) + " " +
             (link_to 'Login', new_user_session_path)
+        end
+    end
+
+    # content helper method
+    def source_helper(layout)
+        if session[:source]
+            content_tag :footer, "Thanks for visiting me  #{session[:source]}, you are on #{layout} layout"
         end
     end
 
